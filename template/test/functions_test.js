2b978dc54f1b189dab61ca011710cdf835f25eb9
KimSvenSand <kim.svensson.sand@gmail.com>
2017-03-02 16:24:02 +0100

    Removed unnessesary dependencies

--- template/test/functions_test.js
+++ template/test/functions_test.js
@@ -2,6 +2,6 @@ import { add } from '../js/functions';
 
 describe('add(a, b)', function() {
   it('should return a + b', function() {
-    expect(functions.add(7, 8)).to.equal(15);
+    expect(add(7, 8)).to.equal(15);
   });
 });
