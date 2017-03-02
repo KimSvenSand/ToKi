9d5ad022d26592f8e64da8a381ba22cf8b8d1907
KimSvenSand <kim.svensson.sand@gmail.com>
2017-03-02 18:12:32 +0100

    Added grunt

---
+++ template/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-browserify");
+
+  // Project configuration.
+  grunt.initConfig({
+    clean: ["dist"],
+
+    copy: {
+      src_to_dist: {
+        cwd: "src",
+        expand: true,
+        src: ["**/*", "**/*.js", "**/*.css"],
+        dest: "dist"
+      }
+    },
+
+    browserify: {
+      dist: {
+        files: {
+          "dist/modules.js": [ "src/js/*.js" ]
+        },
+        options: {
+          transform: [
+            [ "babelify", {
+              plugins: [ "transform-es2015-modules-commonjs" ]
+            } ]
+          ]
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.registerTask("default", ["clean", "copy:src_to_dist", "browserify" ]);
+};
