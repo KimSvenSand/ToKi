57b4ba3aaf945a99f8fd9bcb4fbfe28a49f4be9b
KimSvenSand <kim.svensson.sand@gmail.com>
2017-03-10 14:45:19 +0100

    time and memory measurement

--- template/dist/index.js
+++ template/dist/index.js
@@ -13,27 +13,26 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 'use strict';
 
 console.info('start' + _util2.default.inspect(process.memoryUsage()));
-
 runCalculatorMeasurements(5, 8);
+console.info(_util2.default.inspect(process.memoryUsage()));
 
-global.gc(); //Run garbage collection.
-console.info('end' + _util2.default.inspect(process.memoryUsage()));
+global.gc();
+console.log(_util2.default.inspect(process.memoryUsage()));
 
 function runCalculatorMeasurements(a, b) {
-  var calculator = new _calculator2.default();
-  var result = 0;
-  var t0 = new Date();
-  var hrStart = process.hrtime();
+  let calculator = new _calculator2.default();
+  let result = [];
+  let t0 = new Date();
+  let hrStart = process.hrtime();
 
   for (var i = 0; i < 10000000; i++) {
-    result = calculator.add(i, i + 1);
+    result[i] = calculator.add(i, i + 1);
   }
 
-  var hrEnd = process.hrtime(hrStart);
-  var t1 = new Date();
+  let hrEnd = process.hrtime(hrStart);
+  let t1 = new Date();
 
   console.info(t1 - t0 + 'ms');
   console.info(hrEnd[1] / 1000000 + 'ms');
-  console.info(_util2.default.inspect(process.memoryUsage()));
 }
 //# sourceMappingURL=index.js.map
--- template/src/index.js
+++ template/src/index.js
@@ -3,26 +3,25 @@ import util from 'util';
 'use strict';
 
 console.info('start' + util.inspect(process.memoryUsage()));
-
 runCalculatorMeasurements(5, 8);
+console.info(util.inspect(process.memoryUsage()));
 
-global.gc(); //Run garbage collection.
-console.info('end' + util.inspect(process.memoryUsage()));
+global.gc();
+console.log(util.inspect(process.memoryUsage()))
 
 function runCalculatorMeasurements(a, b) {
-  var calculator = new Calculator();
-  var result = 0;
-  var t0 = new Date();
-  var hrStart = process.hrtime();
+  let calculator = new Calculator();
+  let result = [];
+  let t0 = new Date();
+  let hrStart = process.hrtime();
 
   for (var i = 0; i < 10000000; i++) {
-    result = calculator.add(i, i + 1);
+    result[i] = calculator.add(i, i + 1);
   }
 
-  var hrEnd = process.hrtime(hrStart);
-  var t1 = new Date();
+  let hrEnd = process.hrtime(hrStart);
+  let t1 = new Date();
 
   console.info(t1 - t0 + 'ms');
   console.info(hrEnd[1] / 1000000 + 'ms');
-  console.info(util.inspect(process.memoryUsage()));
 }
