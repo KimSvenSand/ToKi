cb2e437d6ac0aa89a7cbbab6ce18349ffda38a1e
KimSvenSand <kim.svensson.sand@gmail.com>
2017-04-25 19:12:29 +0200

    Moved initiate tree into BinaryTree Class

--- kim-binary-tree-oop/src/index.js
+++ kim-binary-tree-oop/src/index.js
@@ -10,7 +10,8 @@ function runMeasurements(size) {
   var hrStart = process.hrtime();
   var startMem = process.memoryUsage().heapUsed;
   console.info('start:\n' + util.inspect(process.memoryUsage()));
-  var mTree = initiateTree(size);
+  var mTree = new BinarySearchTree(Math.floor((Math.random() * size * 10) + 1));
+  mTree.initiateRandomTree(size);
   console.info('after insert:\n' + util.inspect(process.memoryUsage()));
   mTree.inOrderTraversal();
   console.info('after inOrderTraversal:\n' + util.inspect(process.memoryUsage()));
@@ -19,24 +20,11 @@ function runMeasurements(size) {
   mTree.findNode((Math.random() * size * 2) + 1);
   var endMem = process.memoryUsage().heapUsed;
   console.info('after 3x random findNode:\n' + util.inspect(process.memoryUsage()));
-  
+
   let hrEnd = process.hrtime(hrStart);
   console.info(hrEnd[0] + 's and ' + hrEnd[1] / 1000000 + 'ms');
   console.info('Initial memory usage: ' + startMem);
   console.info('Final memory usage: ' + endMem);
   console.info(hrEnd[0] * 1000 + hrEnd[1] / 1000000);
   console.info(endMem - startMem);
-}
-
-function initiateTree(nrOfElements) {
-  var mRandom = function() {
-    return Math.floor((Math.random() * nrOfElements * 10) + 1);
-  }
-  var tree = new BinarySearchTree(mRandom());
-
-  while (tree.getSize() < nrOfElements) {
-    tree.insert(mRandom());
-  }
-
-  return tree;
 }
